183645807f29a97d72733c92dd643774359812e2
tantecki <tomsz.antecki@gmail.com>
2020-06-17 20:26:59 +0200

    Added music playing feature.

--- HelloWorldHerokuLogic.js
+++ HelloWorldHerokuLogic.js
@@ -1,1 +1,28 @@
-console.log('Hello World!');
+const playMusicButton = document.getElementById('playAudio');
+const audioFile = document.getElementById('audioFile');
+const buttonMessage = document.getElementById('buttonMessage');
+buttonMessage.innerHTML = 'Play Some Music';
+let isMusicPlayed = false;
+const resetButton = document.getElementById('resetAudio');
+resetButton.style.display = 'none';
+
+playMusicButton.addEventListener('click', function () {
+    if (isMusicPlayed) {
+        audioFile.pause();
+        isMusicPlayed = false;
+        buttonMessage.innerHTML = 'Continue';
+    } else {
+        audioFile.play();
+        isMusicPlayed = true;
+        buttonMessage.innerHTML = 'Pause';
+        resetButton.style.display = 'block';
+    }
+});
+
+resetButton.addEventListener('click', function () {
+    audioFile.pause();
+    audioFile.currentTime = 0;
+    isMusicPlayed = false;
+    buttonMessage.innerHTML = 'Play Some Music';
+    resetButton.style.display = 'none';
+});
